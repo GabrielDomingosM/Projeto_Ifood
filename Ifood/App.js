e6a6ff6252a750bcb20e374aab0456f75c03bfaa
GabrielDomingosM <gabrielmarquesha@gmail.com>
2024-06-04 15:08:15 -0300

    App Criado

---
+++ Ifood/App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App melhor que o ifood o indedito doofi </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
