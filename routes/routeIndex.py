2b45d5e8e26fe41505501578979ed713ae95453b
DVzera <davicollescaini@gmail.com>
2024-06-05 13:20:51 -0300

    Usuarios Funcional

--- routes/routeIndex.py
+++ routes/routeIndex.py
@@ -1,12 +1,9 @@
 from routes.usuariosRoutes import usuarioRoute
 from routes.entregadoresRoutes import entregadorRoute
 from routes.home import home
+
 def routeIndex(app):
     usuarioRoute(app=app)
-<<<<<<< HEAD
-    entregadorRoute(app=app)
-=======
     entregadorRoute(app=app)
     home(app)
     
->>>>>>> e6a6ff6252a750bcb20e374aab0456f75c03bfaa
