639fe8ca5f57ba98a7ca90a5b44dbb1c68dac51c
DVzera <davicollescaini@gmail.com>
2024-06-19 14:40:19 -0300

    comita

--- doofi/Screens/Login.js
+++ doofi/Screens/Login.js
@@ -3,7 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StackView } from "@react-navigation/stack";
 import { SafeAreaView, Text, TouchableOpacity, StyleSheet, TextInput } from "react-native";
 
-export default function Login(){
+export default function Login({navigation}){
+
+    function logar(){
+        navigation.navigate("CadEntregadores");
+    }
+
     return(
         <SafeAreaView>
 
@@ -15,7 +20,7 @@ export default function Login(){
                 placeholder="CPF"
             />
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={logar}>
                 <Text>Login</Text>
             </TouchableOpacity>
 
